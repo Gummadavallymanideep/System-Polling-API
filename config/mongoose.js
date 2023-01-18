4a5d5f8f36a7cfae5cc7f6dfc3e48b45899f55f3
Gummadavallymanideep <107338111+Gummadavallymanideep@users.noreply.github.com>
2023-01-18 20:22:10 +0530

    System Polling API

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/System-Polling-API');
+mongoose.connect('mongodb://localhost/api-polling');
 
 const db = mongoose.connection;
 
